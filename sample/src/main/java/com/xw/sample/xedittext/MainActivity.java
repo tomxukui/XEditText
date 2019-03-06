3e75dd08f9a55897ad1b217f9b07185de9509e3e
xukui <152780382@qq.com>
2019-03-06 14:59:20 +0800

    init

--- sample/src/main/java/com/xw/sample/xedittext/MainActivity.java
+++ sample/src/main/java/com/xw/sample/xedittext/MainActivity.java
@@ -6,11 +6,14 @@ import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.xw.repo.XEditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    private XEditText tv_mobile2;
+    private Button btn_mobile2;
     private XEditText clearXEdit;
     private XEditText customXEdit;
     private XEditText showXEdit;
@@ -22,12 +25,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        tv_mobile2 = findViewById(R.id.tv_mobile2);
+        btn_mobile2 = findViewById(R.id.btn_mobile2);
         clearXEdit = findViewById(R.id.clear_marker_edit_text);
         customXEdit = findViewById(R.id.custom_edit_text);
         showXEdit = findViewById(R.id.show_separator_edit_text);
         textView1 = findViewById(R.id.text1);
         textView2 = findViewById(R.id.text2);
         Button button = findViewById(R.id.show_pattern_btn);
+
+        btn_mobile2.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                String mobile = tv_mobile2.getText().toString().trim();
+
+                Toast.makeText(MainActivity.this, mobile, Toast.LENGTH_SHORT).show();
+            }
+
+        });
 
         customXEdit.setPattern(new int[]{4, 4, 4, 4});
 
